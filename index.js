936a5be96c57392aa3c668a5454cc1c303ead2ca
Russo Salvatore <russo.salvatore@osdbsesto>
2021-12-09 11:12:59 +0100

    add

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express=require("express");
 var apiserver=express(); 
+var sf=require("fs");
+const { Z_ASCII } = require("zlib");
 
 
 
@@ -26,4 +28,25 @@ apiserver.get("/mionome",(request,response)=>{
 apiserver.get("/somma",(request,response)=>{          
     console.log("somma request",request.query);
     response.send("risualtato "+(request.query.a-(-request.query.b)));
-})                                                                  
+})  
+apiserver.get("/student",(request,response)=>{          
+    console.log("student id",request.query.id);
+    //leggere file
+    sf.readFile("studenti.js",(err,data)=>{
+        if(err){ console.log("error:"+err);
+    }else{
+        var students=JSON.parse(data);
+        console.log("students:"+students[0].surname);
+        for(var a=0;a<students.length;a++){
+            if(students[a].id==request.query.id){
+                response.send("lo studente è: "+students[a].id+" "+students[a].surname+" "+students[a].name); 
+            }
+        }
+        response.send(students.find(x=>x.id===request.query.id));
+    }
+
+});
+    //prelevare l'oggetto con id 1
+    //send
+
+})                                                              
---
+++ studenti.js
@@ -0,0 +1,12 @@
+[
+{
+"surname":"agostinelli",
+"name":"Luca",
+"id":1
+},
+{
+    "surname":"alampi",
+    "name":"andrian",
+    "id":2    
+}
+]
